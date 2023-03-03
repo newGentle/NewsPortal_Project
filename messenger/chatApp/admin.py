ec2e345d9a6842ece62ef1aeff37ecdcdca1f84d
Akbar <abdullaev.akbar@gmail.com>
2023-03-03 17:28:39 +0500

    Delete messenger directory

--- messenger/chatApp/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-
-from .models import Rooms, Messages, Chat_Users
-
-admin.site.register(Rooms)
-admin.site.register(Messages)
-admin.site.register(Chat_Users)
